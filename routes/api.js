591cf1cdf9fc809dca71d662a1e19e446232779d
Helder S Ribeiro <hsribei.pub@gmail.com>
2018-04-23 14:15:08 -0400

    Test and implement thread creation

--- routes/api.js
+++ routes/api.js
@@ -8,10 +8,18 @@
 
 "use strict";
 
-var expect = require("chai").expect;
+const { Thread, Reply } = require("../models");
 
 module.exports = function(app) {
-  app.route("/api/threads/:board");
+  app.route("/api/threads/:board").post(async (req, res) => {
+    const newThread = {
+      board: req.params.board,
+      text: req.body.text,
+      delete_password: req.body.delete_password
+    };
+    const savedThread = await Thread.create(newThread);
+    res.json(savedThread);
+  });
 
   app.route("/api/replies/:board");
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,16 +6,47 @@
 *       (if additional are added, keep them at the very end!)
 */
 
-var chaiHttp = require("chai-http");
-var chai = require("chai");
-var assert = chai.assert;
-var server = require("../server");
+const chaiHttp = require("chai-http");
+const chai = require("chai");
+const _ = require("lodash");
+const fp = require("lodash/fp");
+const assert = chai.assert;
+const server = require("../server");
 
 chai.use(chaiHttp);
 
+const newAsFn = type => (...args) => new type(...args);
+const isDateFromString = fp.compose(_.isDate, newAsFn(Date));
+
 suite("Functional Tests", function() {
   suite("API ROUTING FOR /api/threads/:board", function() {
-    suite("POST", function() {});
+    suite("POST", function() {
+      test("Create valid thread and typecheck result", async function() {
+        const board = "test";
+        const text = "test";
+        const delete_password = "test";
+        const res = await chai
+          .request(server)
+          .post(`/api/threads/${board}`)
+          .send({ text, delete_password });
+
+        assert.strictEqual(res.status, 200);
+        const savedThread = res.body;
+        const fieldTypes = {
+          _id: _.isString,
+          text: _.isString,
+          created_on: isDateFromString,
+          bumped_on: isDateFromString,
+          reported: _.isBoolean,
+          delete_password: _.isString,
+          replies: _.isArray
+        };
+        Object.keys(fieldTypes).forEach(field => {
+          assert.property(savedThread, field);
+          assert(fieldTypes[field](savedThread[field]));
+        });
+      });
+    });
 
     suite("GET", function() {});
 
