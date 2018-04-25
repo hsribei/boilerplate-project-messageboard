035a17b6e705167a9389298e516872e4bc019347
Helder S Ribeiro <hsribei.pub@gmail.com>
2018-04-25 15:44:46 -0400

    Alphabetize requires

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 "use strict";
 
-const express = require("express");
 const bodyParser = require("body-parser");
-const expect = require("chai").expect;
 const cors = require("cors");
+const express = require("express");
+const expect = require("chai").expect;
 
 const apiRoutes = require("./routes/api.js");
 const fccTestingRoutes = require("./routes/fcctesting.js");
